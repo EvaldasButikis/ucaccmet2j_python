afa25568015e0646fb3427ca24426790286c89be
Evaldas <butikisevaldas@gmail.com>
2025-01-14 18:06:12 +0100

    exercise 3 is finished, as am i

--- Calculations_Exercise3.py
+++ Calculations_Exercise3.py
@@ -1,58 +1,71 @@
 import json
 import csv
 
-# opens the stations csv file for reading
-csvfile = open("stations.csv")
+# This will load in the stations csv as a list of dictionaries
+with open("stations.csv") as file:
+    station_codes = list(csv.DictReader(file))
 
-# creates a new v
-locations = csv.DictReader(csvfile)
-
-station_codes = []
-
-#for row in locations:
-#    station_codes.append(row["Station"])
-
-#print(station_codes)
+# Defines and splits all the months so we can use it for our "starts with" function
+all_dates = "01 02 03 04 05 06 07 08 09 10 11 12".split()
 
 # Loads the .json contents into a variable
 with open("precipitation.json") as file:
     measurements = json.load(file)
 
-# Creates empty dictionary to store the filtered station dictionaries in
-Seattle_precipitation = {}
+# Creates an empty dictionary to store the precipitation calculation results of separate cities
+results = {}
 
-# Defines and splits all the months so we can use it for our "starts with" function
-all_dates = "01 02 03 04 05 06 07 08 09 10 11 12".split()
-# Defines empty dictionary for precipitation per month
-precipitation_per_month = {}
+cities_yearly_precipitation = 0
 
-# Creates empty dictionary for relative precepitation per month
-relative_monthly_precipitation = {}
+# For loop for each individual station's precipitation observations
+for station in station_codes:
+    station_code = station['Station']
 
-# Defines empty keys within the "precipitaton_per_month" dictionary
-for date in all_dates:
-    precipitation_per_month[date] = 0
-    relative_monthly_precipitation[date] = 0
+    # Creates empty dictionary to store the filtered station dictionaries in
+    city_precipitation = {}
 
-# Defines variable for total yearly precipitation for later use in loop
-total_yearly_precipitation=0
+    # Defines variable for total yearly precipitation for later use in loop
+    total_yearly_precipitation=0
+    
+    # Defines empty dictionary for precipitation per month
+    precipitation_per_month = {}
 
-# For loop to filter specifically the Seattle station observations into the empty list from before
-for stations in measurements:
-    for row in locations:
-        station_code = row("Station")
-    if stations["station"] == "GHCND:US1WAKG0038":
-        total_yearly_precipitation += stations["value"]
-        for date in all_dates:
-            if stations["date"].startswith(f"2010-{date}"):
-                precipitation_per_month[date] += stations["value"]
-            relative_monthly_precipitation[date] = precipitation_per_month[date] / total_yearly_precipitation
-        Seattle_precipitation["precipitation_per_month"] = precipitation_per_month # Adds the above calculations into the larger dictionary so it's more verbose
-        Seattle_precipitation["total_yearly_precipitation"] = total_yearly_precipitation
-        Seattle_precipitation["relative_monthly_precipitation"] = relative_monthly_precipitation
+    # Creates empty dictionary for relative precepitation per month
+    relative_monthly_precipitation = {}
+
+    # Defines empty keys within the "precipitaton_per_month" dictionary
+    for date in all_dates:
+        precipitation_per_month[date] = 0
+        relative_monthly_precipitation[date] = 0
+
+    for stations in measurements:
+        if stations["station"] == station_code:
+            total_yearly_precipitation += stations["value"]
+            for date in all_dates:
+                if stations["date"].startswith(f"2010-{date}"):
+                    precipitation_per_month[date] += stations["value"]
+
+    for date in precipitation_per_month:
+        relative_monthly_precipitation[date] = precipitation_per_month[date] / total_yearly_precipitation
+    
+    city_precipitation["precipitation_per_month"] = precipitation_per_month # Adds the above calculations into the larger dictionary so it's more verbose
+    city_precipitation["total_yearly_precipitation"] = total_yearly_precipitation
+    city_precipitation["relative_monthly_precipitation"] = relative_monthly_precipitation
+
+    cities_yearly_precipitation += city_precipitation["total_yearly_precipitation"] 
+
+    results[station["Location"]] = city_precipitation
+
+for station in station_codes:
+    city_precipitation = results[station["Location"]]
+
+    relative_yearly_precipitation = city_precipitation["total_yearly_precipitation"] / cities_yearly_precipitation
+    city_precipitation["relative_yearly_precipitation"] = relative_yearly_precipitation
+
+    results[station["Location"]] = city_precipitation
 
 #print(Seattle_precipitation)
 
 # Saves the outputted results into a .json file
 with open('results.json', 'w', encoding='utf-8') as file:
-    json.dump(Seattle_precipitation, file, indent = 4)
+    json.dump(results, file, indent = 2)
